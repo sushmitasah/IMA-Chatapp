6e2100a66e3e8bbcba68703134f4a0f7f497812d
Sushmita Sah <39860307+sushmitasah@users.noreply.github.com>
2019-02-04 05:11:25 +0530

    app.js file added-made by sushmita.
    This is the .js extension file associated with chatpage.html.

---
+++ app.js
@@ -0,0 +1,15 @@
+var app = angular.module('chatApp', ['firebase']);
+
+app.controller('ChatController', function($scope, $firebaseArray) {
+    var ref = firebase.database().ref().child('messages');
+    $scope.messages = $firebaseArray(ref);
+
+    $scope.send = function() {
+        $scope.messages.$add({
+        	namef:$scope.names,
+            message: $scope.messageText,
+            date: Date.now()
+        })
+    }
+
+})
